6dd44cd1b3bd93ad6d5a290942a6fdb85b0dcd21
MinhChau204 <chauuu16102004@gmail.com>
2025-04-28 22:32:43 +0700

    update bt

---
+++ Thuchanh2/app/src/main/java/com/example/thuchanh2/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.thuchanh2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        val TextView = findViewById<TextView>(R.id.tv_name)
+        val Button = findViewById<Button>(R.id.btn_namee)
+
+        Button.setOnClickListener{
+            TextView.text = "I'm Minh Chau"
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
